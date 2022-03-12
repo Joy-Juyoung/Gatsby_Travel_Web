c24b7e67042a28c2e0b662a1c8930eb42fb97f6d
izoilj <ljy9407@gmail.com>
2022-03-12 11:12:22 -0700

    All done but some error at SatsData

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,22 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import React from "react"
+// import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Hero from "../components/Hero"
+import Trips from "../components/Trips"
+import Testimonials from "../components/Testimonials"
+import Stats from "../components/Stats"
+import Email from "../components/Email"
 
+// 리액트에 App.js와 같은 느낌?
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <Hero />
+    <Trips heading="Our Favorite Destinations" />
+    <Testimonials />
+    <Stats />
+    <Email />
   </Layout>
 )
 
